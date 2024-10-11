7d09247f5ceef64cd3e7cdc737bc0a0352ec8780
Ethan <EthanAEdwards5@outlook.com>
2024-10-11 16:13:09 -0600

    remove home

--- ui/src/app/pages/chat/chat.component.ts
+++ ui/src/app/pages/chat/chat.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LayoutComponent],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
 export class ChatComponent {
+  messages: ChatMessage[] = [];
 
+  sendMessage(content: string) {
+    if (content.trim() !== '') {
+      this.addMessage(content, true);
+      // Simulate a response (replace with actual API call in a real app)
+      setTimeout(() => this.addMessage(`I received: ${content}`, false), 1000);
+      console.log(this.messages);
+    }
+  }
+
+  private addMessage(content: string, isUser: boolean) {
+    this.messages.push({ content, isUser, token: 50 });
+  }
+}
+
+export interface ChatMessage {
+  content: string;
+  isUser: boolean;
+  token: number;
 }
--- ui/src/app/pages/layout/layout.component.ts
+++ ui/src/app/pages/layout/layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ChatComponent } from '../chat/chat.component';
-import { HomeComponent } from '../home/home.component';
 import { ModalSettingsComponent } from '../../shared/modal-settings/modal-settings.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [ChatComponent, HomeComponent, ModalSettingsComponent],
+  imports: [ChatComponent, ModalSettingsComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
