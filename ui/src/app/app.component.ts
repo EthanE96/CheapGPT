42a090ecb11b01c6a4eaa151bf2e81226ddf91dd
Ethan <EthanAEdwards5@outlook.com>
2024-12-21 16:23:21 -0700

    routes

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './pages/layout/layout.component';
 import { themeChange } from 'theme-change';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'CheapGPT';
+
   constructor() {
     themeChange();
   }
--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { SignupComponent } from './pages/account/signup/signup.component';
+import { NotFoundComponent } from './pages/errors/not-found/not-found.component';
+import { LoginComponent } from './pages/account/login/login.component';
+import { LogoutComponent } from './pages/account/logout/logout.component';
 
 export const routes: Routes = [
-  // { path: 'password', component: PasswordComponent },
-  // //other routes
-  // { path: '', redirectTo: '/entries', pathMatch: 'full' },
-  // { path: '**', component: NotFoundComponent },
+  { path: '', component: LayoutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'logout', component: LogoutComponent },
+
+  // wildcard route for a 404
+  { path: '**', component: NotFoundComponent },
 ];
